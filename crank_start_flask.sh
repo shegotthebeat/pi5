5ce74371b8d72872f163ebc5c8aa906878604025
shegotthebeat <nematode@shegotthebeat.com>
2025-08-04 21:11:21 -0400

    Update crank_start_flask.sh

--- crank_start_flask.sh
+++ crank_start_flask.sh
@@ -21,7 +21,7 @@ if [ -f "requirements.txt" ]; then
 fi
 
 # Set FLASK_APP and run the app in the background
-export FLASK_APP=app.py
+export FLASK_APP=csvboxer.py
 echo "Starting csvboxer app on port 5000..."
 flask run --host=0.0.0.0 --port=5000 &
 
@@ -49,7 +49,7 @@ if [ -f "requirements.txt" ]; then
 fi
 
 # Set FLASK_APP and run the app in the background
-export FLASK_APP=app.py
+export FLASK_APP=filer.py
 echo "Starting filer app on port 5001..."
 flask run --host=0.0.0.0 --port=5001 &
 
